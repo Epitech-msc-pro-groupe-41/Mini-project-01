13406b8a65e5ddfb9252518b580608da9153d30d
Carolina Niranjala Louis Thayanand <clouisthayanand@gmail.com>
2019-09-12 11:27:43 +0200

    Fix CORS errors + add ChartManager route

--- api/src/main/java/com/timemanager/core/src/controller/ChartManagerController.java
+++ api/src/main/java/com/timemanager/core/src/controller/ChartManagerController.java
@@ -1,10 +1,17 @@
 package com.timemanager.core.src.controller;
 
+import com.timemanager.core.src.dto.ChartResponseDto;
+import com.timemanager.core.src.service.ChartManagerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 
 @Api(value = "ChartManager", consumes = MediaType.APPLICATION_JSON_VALUE, tags = "ChartManager")
@@ -12,4 +19,14 @@ import io.swagger.annotations.Api;
 @RequestMapping("/chartManager")
 public class ChartManagerController {
 
+    @Autowired
+    ChartManagerService chartManagerService;
+    
+    @ApiOperation(value = "Get/Refresh data for charts (chart 1 : working hours by day")
+    @RequestMapping(method = RequestMethod.GET, value = "/{userID}")
+    public ChartResponseDto getChartData(
+        @PathVariable(name = "userID", required = true) String userID) {
+
+        return chartManagerService.getChartData(userID);
+    }
 }
